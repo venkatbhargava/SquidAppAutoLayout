c084c6072e850de1dc25348af149a9f78cea336f
Venkata Bhargav <bhargav.venkata.a@gmail.com>
2020-06-14 14:18:33 +0530

    Initial Commit

---
+++ SquidApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SquidApp
+//
+//  Created by Venkata Bhargav on 14/06/20.
+//  Copyright © 2020 venkat bhargav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
